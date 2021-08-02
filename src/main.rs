278071f88a0d17ad871f123ebf0aa988859d1ad9
mzyzc <mzyzc@czyz.xyz>
2021-08-02 13:35:55 +0100

    Looped UI updates

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,30 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(io::stdout());
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|frame| {
-        let chunks = widgets::get_chunks()
-            .split(frame.size());
+    loop {
+        terminal.draw(|frame| {
+            let chunks = widgets::get_chunks()
+                .split(frame.size());
 
-        let top = widgets::get_top_chunk()
-            .split(chunks[0]);
+            let top = widgets::get_top_chunk()
+                .split(chunks[0]);
 
-        let bottom = widgets::get_bottom_chunk()
-            .split(chunks[1]);
+            let bottom = widgets::get_bottom_chunk()
+                .split(chunks[1]);
 
-        let timescale = widgets::get_timescale_tabs();
-        frame.render_widget(timescale, top[0]);
+            let timescale = widgets::get_timescale_tabs();
+            frame.render_widget(timescale, top[0]);
 
-        let change = widgets::get_change_block();
-        frame.render_widget(change, top[1]);
+            let change = widgets::get_change_block();
+            frame.render_widget(change, top[1]);
 
-        let graph = widgets::get_graph_block();
-        frame.render_widget(graph, bottom[0]);
+            let graph = widgets::get_graph_block();
+            frame.render_widget(graph, bottom[0]);
 
-        let exchange = widgets::get_exchange_block();
-        frame.render_widget(exchange, bottom[1]);
-    })?;
+            let exchange = widgets::get_exchange_block();
+            frame.render_widget(exchange, bottom[1]);
+        })?;
+    }
 
     Ok(())
 }
